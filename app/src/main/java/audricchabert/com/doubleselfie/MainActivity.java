ab429352af5c30a384a70f8292c9daba8f63db0a
Audric Chabert <audric.chabert@outlook.com>
2017-10-24 21:43:36 +0200

    test camera settings

--- app/src/main/java/audricchabert/com/doubleselfie/MainActivity.java
+++ app/src/main/java/audricchabert/com/doubleselfie/MainActivity.java
@@ -1,19 +1,31 @@
 package audricchabert.com.doubleselfie;
 
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.hardware.camera2.CameraManager;
+import android.widget.TextView;
 
+@TargetApi(23)
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -26,8 +38,63 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        //first change : this comment
+//my code
+
+        TextView t = (TextView)findViewById(R.id.myTextView);
+
+
+
+        Context c = getApplicationContext();
+        CameraManager cameraManager = c.getSystemService(CameraManager.class);
+        String[] cameraIdList = null;
+        try{
+            cameraIdList = cameraManager.getCameraIdList();
+        }catch(CameraAccessException cae){
+            //todo : find log function
+            Log.e(TAG,"CameraAccessException getCameraIdList");
+        }
+
+        if(cameraIdList == null){
+            Log.e(TAG,"cameraIdList is null");
+        }else{
+            for (String cameraId :
+                    cameraIdList) {
+                t.setText(t.getText()+cameraId);
+                Log.e(TAG,"debug : cameraId"+cameraId);
+                try {
+                    CameraCharacteristics cc = cameraManager.getCameraCharacteristics(cameraId);
+                    Log.e(TAG,"debug : cameraCharacteristics"+cc);
+
+                }catch(CameraAccessException cae){
+                    Log.e(TAG,"CameraAccessException getCameraCharacteristics");
+                }
+            }
+        }
+
+        if(checkCameraHardware(c)){
+            Log.e(TAG,"debug : hasCameraHardware");
+        }
+
+
+//end my code
+
+
     }
+
+    //my code
+
+    /** Check if this device has a camera */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+
+    //end my code
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
